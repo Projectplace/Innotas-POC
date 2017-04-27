533612c99f5918e39182ab32a9ebd64565dfc877
Jim Brännlund <jimbrannlund@fastmail.com>
2017-04-27 11:05:42 +0200

    first commit

---
+++ test_automation/configs/user.js
@@ -0,0 +1,6 @@
+user_pref("browser.download.lastDir", "/tmp");  // default download dir
+user_pref("browser.download.folderList", 2);  // activates browser.download.lastDir
+user_pref("browser.helperApps.neverAsk.saveToDisk", "application/msword");
+user_pref("browser.helperApps.deleteTempFileOnExit", true);
+user_pref("browser.cache.check_doc_frequency", 1);  // turn off caching
+user_pref("devtools.selfxss.count", 20);  // allow pasting in console
